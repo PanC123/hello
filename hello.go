904bfa69c4ff4073c3bb9409bbfd3435464f0c47
cheng.pan <a765567450@icloud.com>
2023-01-10 12:13:36 +0800

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main // import "github.com/PanC123/hello"
+
+import (
+        "fmt"
+        "github.com/davyxu/cellnet"
+        "rsc.io/quote"
+)
+
+func main() {
+        fmt.Println(quote.Hello())
+}
